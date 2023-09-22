2bb69f4207042aefa0f76f6cd8ba9cfc38230c21
Barna Gergely <84261815+BarnaGergely@users.noreply.github.com>
2023-09-22 10:42:21 +0200

    init

---
+++ C#/C#_Jegyzet.cs
@@ -0,0 +1,19 @@
+#region 2. óra
+
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+#endregion
+
+#region 3. óra - 
+
+
+
+#endregion
